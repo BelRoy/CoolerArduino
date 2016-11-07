358eb34da4866441b5a2c13bb6e52f2433e3b9db
Vladislav Belous <oksiwhite@ukr.net>
2016-11-07 14:54:53 +0200

    Sample

---
+++ kuller_arduino.ino
@@ -0,0 +1,21 @@
+
+volatile int val;
+
+void rpm(){
+  val++;
+}
+
+void setup() {
+  Serial.begin(9600);
+  attachInterrupt(0, rpm, CHANGE);
+
+}
+
+void loop() {
+  val = 0;
+  delay(500);
+  Serial.print((val*60)/2);
+  Serial.print(" rpm ");
+  Serial.print(val);
+  Serial.println(" val ");
+}
